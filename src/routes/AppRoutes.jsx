fa905648fba12d8b50a53bd2c13f34a0b3f0b19d
Wilkson Flor <wilksonflor12@gmail.com>
2025-01-02 12:02:59 -0300

    fix: erro no build

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,14 +1,13 @@
-import React from "react";
+// import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { MainLayout } from "../layouts/MainLayout";
+import { AuthProvider } from "../context/AuthContext";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Produtos from "../pages/Produtos/Produtos";
 import Categorias from "../pages/Categorias/Categorias";
-import Usuarios from "../pages/Usuarios/Usuarios";
 import Login from "../pages/Login/Login";
-import { AuthProvider } from "../context/AuthContext";
 import ProtectedRoute from "./ProtectedRoute";
-
+// import Usuarios from "../pages/Usuarios/Usuarios";
 const AppRoutes = () => {
   return (
     <AuthProvider>
@@ -26,7 +25,7 @@ const AppRoutes = () => {
             <Route index element={<Dashboard />} />
             <Route path="produtos" element={<Produtos />} />
             <Route path="categorias" element={<Categorias />} />
-            <Route path="usuarios" element={<Usuarios />} />
+            {/* <Route path="usuarios" element={<Usuarios />} /> */}
           </Route>
         </Routes>
       </Router>
